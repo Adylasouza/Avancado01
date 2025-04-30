b0e78b4a8d899f268da5e7ea2d4a3026447e5266
Adyla Souza <adyla.projetos@gmail.com>
2025-04-30 15:10:19 -0300

    piramide

--- Biblioteca.py
+++ Biblioteca.py
@@ -7,3 +7,20 @@ def piramide(num):
         for x in range(0, i):
             print(i, end=" ")
         print()
+
+def contVogais(texto):
+    '''código que fiz
+    texto = "O rato roeu a roupa do rei de Roma"
+    vogais= "a", "e", "i" , "o", "u"
+    soma = 0
+
+    for x in range(0,(len(texto))):
+        if texto == vogais:
+            soma= vogais+1
+        print(soma)'''
+
+    contar= 0
+    for x in range (len(texto)):
+        if texto[x]  == "a" or texto[x] == "e" or texto[x]=="i" or texto[x] =="o" or texto[x]=="u":
+            contar=contar+1
+    print(contar)
---
+++ Exercicio03.py
@@ -0,0 +1,5 @@
+from Biblioteca import *
+#input("Digite o texto: ")
+texto= "o rato roeu a roupa do rei de Roma"
+contVogais(texto)
+
